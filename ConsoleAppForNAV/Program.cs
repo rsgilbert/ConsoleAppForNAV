50b20c1b8681a48f06bd25f38da8d6c5e6c07a15
Gilboot <ssenyonjogilbert@gmail.com>
2021-06-29 10:54:31 +0300

    Add project files.

---
+++ ConsoleAppForNAV/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForNAV
+{
+    using CustomerService;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instance of service and set credentials
+            PageWithCapitalization_Service service = new PageWithCapitalization_Service();
+            service.UseDefaultCredentials = true;
+/*
+            // Create instance of customer
+            PageWithCapitalization cust = new PageWithCapitalization();
+            cust.Name = "Ssenyo Gil";
+            cust.Address = "Michigan Tower";
+
+            // Insert customer using default operation
+            service.Create(ref cust);
+            Console.WriteLine("created customer " + cust.Name);
+
+            // Calls extension codeunit to capitalize customer name
+            // It takes the customer record as parameter
+            service.CapitalizeName(cust.Key);
+
+            // Refreshes customer
+            cust = service.Read(cust.No);
+ 
+            Console.WriteLine("Refreshed customer is " + cust.Name);
+*/
+            // delete created customer so this same code can be run again with no issues 
+            /* bool isDeleted = service.Delete(cust.Key);
+             Console.WriteLine("Deleted: " + isDeleted);
+
+             cust = service.Read("C00040");
+             service.Delete(cust.Key);
+            */
+
+            PageWithCapitalization_Filter filter = new PageWithCapitalization_Filter();
+            filter.Field = PageWithCapitalization_Fields.Name;
+            filter.Criteria = "SSE*";
+            PageWithCapitalization_Filter[] filters = new PageWithCapitalization_Filter[] { filter };
+            PageWithCapitalization[] customers = service.ReadMultiple(filters, null, 10);
+            for(int i = 0;i < customers.Length; i++)
+            {
+                PageWithCapitalization c = customers[i];
+                Console.WriteLine("Customer name is " + c.Name + " and address is " + c.Address);
+            }
+
+
+
+
+        }
+    }
+}
